19a504e93f8f8987c17cee52a1fd7368d236be40
vfomenko <vladislav.fomenko.2014@gmail.com>
2023-07-09 20:17:30 +0300

    Change DB on SQLite

--- ForumFiles/app/settings.py
+++ ForumFiles/app/settings.py
@@ -72,23 +72,23 @@ ALLOWED_HOSTS = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "AskHeroes",
-        "USER": "vfomenko",
-        "PASSWORD": "password",
-        "HOST": "localhost",
-        "PORT": "3306",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "AskHeroes",
+#         "USER": "vfomenko",
+#         "PASSWORD": "password",
+#         "HOST": "localhost",
+#         "PORT": "3306",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
